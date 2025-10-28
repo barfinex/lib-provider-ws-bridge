46d3f268e6cc62a78126e5686e64a7cdf962b9b7
barfinex-ci <ci@barfinex.com>
2025-10-28 18:58:28 +0000

    fix(provider-ws-bridge): trigger release

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export * from './lib/socket.gateway';
 export * from './lib/ws-bridge.module';
 export * from './lib/utils/url';
 // export * from './lib/swagger';
+
 export * from './lib/socket-adapter';
 
